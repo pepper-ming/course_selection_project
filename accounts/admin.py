79fafacfff82c1bdaab8f0332458ca0c72d462be
pepper-ming <pepper.ming.1128@gmail.com>
2025-06-05 14:21:30 +0800

    新增使用者管理功能，註冊 UserAdmin 類別以自訂顯示欄位及過濾器

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'name', 'role', 'email', 'is_active')
+    list_filter = ('role', 'is_active', 'is_staff')
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('額外資訊', {'fields': ('name', 'role')}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('額外資訊', {'fields': ('name', 'role', 'email')}),
+    )
